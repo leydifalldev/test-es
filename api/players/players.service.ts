0a4c8744d9fbb4cc3ea15299503ef7e6a381b64f
leydifalldev <doudou.leydi.fall@gmail.com>
2019-09-25 23:31:28 +0200

    Question 3 -> request data from url

--- api/players/players.service.ts
+++ api/players/players.service.ts
@@ -1,7 +1,11 @@
 import * as playerdata from "./players.data.json";
+import http from "http";
+import axios from "axios";
+
 class PlaceService {
-  getList() {
-    return playerdata.players.sort((a, b)=> {
+  async getList() {
+    const playerdata = await this.getDataFromEndPoint();
+    return playerdata.data.players.sort((a, b)=> {
       if (a.id !== b.id) {
         return a.id - b.id
     }
@@ -12,12 +16,21 @@ class PlaceService {
     });
   }
 
-  getPlayer(ref) {
-    let player = playerdata.players.find(player => player.id == ref);
+  async getPlayer(ref) {
+    const playerdata = await this.getDataFromEndPoint();
+    let player = playerdata.data.players.find(player => player.id == ref);
     let status = player ? 200 : 404;
     return {
       status: status,
       data: player
+    }
+  }
+
+  async getDataFromEndPoint(){
+    try{
+      return await axios.get("https://eurosportdigital.github.io/eurosport-node-developer-recruitment/headtohead.json");
+    } catch(e){
+      console.log(e)
     }
   }
 }
